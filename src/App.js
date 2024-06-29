4e2085b1ca98d472660c14f45ee869971108e041
Daniel <danypetrov2002@gmail.com>
2024-06-30 01:55:59 +0300

    Update App.js, index.js and add ErrorBoundary.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ErrorBoundary from './ErrorBoundary';
 import { Container, Box, Typography, TextField, IconButton, Avatar, Grid, Card, CardContent, CardMedia, Button } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -19,63 +20,65 @@ const products = [
 
 function App() {
   return (
-    <Container>
-      {/* Верхняя панель */}
-      <Box display="flex" justifyContent="space-between" alignItems="center" py={2}>
-        <Typography variant="h5">Electronics</Typography>
-        <Box display="flex" alignItems="center">
-          <TextField placeholder="Search" variant="outlined" size="small" />
-          <IconButton>
-            <SearchIcon />
-          </IconButton>
-          <IconButton>
-            <ShoppingCartIcon />
-          </IconButton>
-          <IconButton>
-            <Avatar>
-              <AccountCircleIcon />
-            </Avatar>
-          </IconButton>
+    <ErrorBoundary>
+      <Container>
+        {/* Верхняя панель */}
+        <Box display="flex" justifyContent="space-between" alignItems="center" py={2}>
+          <Typography variant="h5">Electronics</Typography>
+          <Box display="flex" alignItems="center">
+            <TextField placeholder="Search" variant="outlined" size="small" />
+            <IconButton>
+              <SearchIcon />
+            </IconButton>
+            <IconButton>
+              <ShoppingCartIcon />
+            </IconButton>
+            <IconButton>
+              <Avatar>
+                <AccountCircleIcon />
+              </Avatar>
+            </IconButton>
+          </Box>
         </Box>
-      </Box>
 
-      {/* Категории */}
-      <Box py={2}>
-        <Typography variant="h6">Categories</Typography>
-        <Grid container spacing={2}>
-          {categories.map(category => (
-            <Grid item key={category.id}>
-              <Button variant="contained" style={{ backgroundColor: category.color }}>
-                {category.name}
-              </Button>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+        {/* Категории */}
+        <Box py={2}>
+          <Typography variant="h6">Categories</Typography>
+          <Grid container spacing={2}>
+            {categories.map(category => (
+              <Grid item key={category.id}>
+                <Button variant="contained" style={{ backgroundColor: category.color }}>
+                  {category.name}
+                </Button>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
 
-      {/* Список продуктов */}
-      <Box py={2}>
-        <Grid container spacing={2}>
-          {products.map(product => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
-              <Card>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={`/${product.image}`}  // Убедитесь, что изображения доступны по этому пути
-                  alt={product.name}
-                />
-                <CardContent>
-                  <Typography variant="h6">{product.name}</Typography>
-                  <Typography variant="body2" color="textSecondary">${product.price}</Typography>
-                  <Typography variant="body2" color="textSecondary">Rating: {product.rating} ⭐</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </Container>
+        {/* Список продуктов */}
+        <Box py={2}>
+          <Grid container spacing={2}>
+            {products.map(product => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
+                <Card>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={`/${product.image}`}  // Убедитесь, что изображения доступны по этому пути
+                    alt={product.name}
+                  />
+                  <CardContent>
+                    <Typography variant="h6">{product.name}</Typography>
+                    <Typography variant="body2" color="textSecondary">${product.price}</Typography>
+                    <Typography variant="body2" color="textSecondary">Rating: {product.rating} ⭐</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Container>
+    </ErrorBoundary>
   );
 }
 
